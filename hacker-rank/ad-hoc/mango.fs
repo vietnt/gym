5a9ad873be243fc53beddc53abfe5b3b63c5a7a3
vietnt <viet2nt@live.com>
2017-11-07 03:35:39 +0700

    Create mango.fs

---
+++ hacker-rank/ad-hoc/mango.fs
@@ -0,0 +1,30 @@
+// https://www.hackerrank.com/challenges/mango/problem
+
+open System
+
+let readArray() = Console.ReadLine().Split ' ' |> Array.map int64 
+
+let [| n; m |] = readArray()
+let a = readArray()
+let h = readArray()
+
+let g a m e = 
+    e
+    |> Seq.map (fun (a,b) -> a + (m - 1L)*b)
+    |> Seq.sort
+    |> Seq.truncate (int m)
+    |> Seq.sum
+    |> ((>=) a)
+
+let rec f m a b s =
+    if a = b then a
+    else
+       let t = (a + b + 1L)/2L
+       if g m t s then f m t b s
+       else f m a (t - 1L) s
+        
+
+Seq.zip a h
+|> Seq.sortWith (fun (a,x) (b,y) -> compare x y)
+|> f m 0L n
+|> printfn "%i"
